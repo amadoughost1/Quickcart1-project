6ab513583f29beffa25ef69348bb1d7f0979db21
amadoughost1 <amadougayeniang@gmail.com>
2025-08-06 19:19:20 +0000

    my changes

--- models/User.js
+++ models/User.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    _id: {
-        type: String, required: true
-    },
-    name: {
-        type: String, required: true
-    },
-    email: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    cartItems: {
-        type: Object, default: {} }
-    }, { minimize: false })
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  cartItems: { type: Object, default: {} },
+}, { minimize: false });
 
-    const User = mongoose.models.user || mongoose.model('user',userSchema)
+const User = mongoose.models.user || mongoose.model('user', UserSchema);
+//                         ici ------------------------------------^^^^^^
 
 export default User;
